b0ec51b65e6d199be8984cff80208825bd126c4a
EgorChizhik <legend2019rus@gmail.com>
2026-06-12 22:16:51 +0400

    fix: настроена отдача index.html для корня сайта

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView  # <-- Добавили импорт для отображения React
 from rest_framework.routers import DefaultRouter
 from services.views import CategoryViewSet, ServiceViewSet
 from barbers.views import BarberViewSet, BarberTimeSlotListView
@@ -16,11 +17,15 @@ router.register(r'appointments', AppointmentViewSet)
 router.register(r'gallery', WorkViewSet)
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/barbers/<int:barber_id>/slots/', BarberTimeSlotListView.as_view(), name='barber-slots'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 admin.site.index_title = "Управление системой «Van Blade»"
